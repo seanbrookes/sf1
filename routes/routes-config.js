88747ca77fab3ef8f3feff9009458f90a5ab71a5
seanbrookes <seanbrookes@beachair.ca>
2013-10-03 00:26:36 -0700

    removing not core fraemwork modules and code

--- routes/routes-config.js
+++ routes/routes-config.js
@@ -8,7 +8,7 @@
 var UserModel = require('./../models/user-model');
 var user = require('./user');
 var admin = require('./admin');
-var posts = require('./posts');
+
 module.exports = function(app){
 
     /*
@@ -22,21 +22,5 @@ module.exports = function(app){
     app.post('/user',user.createUser);
     app.get('/pendingaccounts',admin.getPendingAccountList);
 
-    /*
-    *
-    * Posts
-    *
-    * */
-    app.post('/posts',posts.addPost);
-    app.get('/publishedposts',posts.getPublishedPosts);
-    app.get('/recentposts',posts.getRecentPosts);
 
-    app.get('/userposts/:userId',posts.getUserPosts);
-    app.get('/posts',posts.getPosts);
-    app.get('/posts/:id',posts.getPost);
-    app.get('/postbyslug/:slug',posts.getPostBySlug);
-    app.put('/publishpost/:id',posts.publishPost);
-    app.put('/posts/:id',posts.updatePost);
-    //app.put('/publishpost/:id',posts.publishPost);
-    app.delete('/posts/:id',posts.deletePost);
  }
