4910cdbc598cfab471e8ad252c8c6fa451f29156
seanbrookes <seanbrookes@beachair.ca>
2013-10-03 19:58:55 -0700

    fix up and refactor home view

--- public/modules/home/home.controller.js
+++ public/modules/home/home.controller.js
@@ -6,33 +6,29 @@
  * Time: 10:57 PM
  *
  */
-define(['sf1','modules/home/home.models','modules/home/home.views','text!modules/home/home.templates.html'],
-    function(sf1, Model, View, template){
-        var anchorSelector = '#TemplateContainer';
+define(['sf1', 'modules/home/home.models', 'modules/home/home.views', 'text!modules/home/home.templates.html'],
+  function (sf1, Model, View, template) {
+    var anchorSelector = '#TemplateContainer';
 
-        _.templateSettings.variable = 'S';
-        var baseMarkup = $(template);
-        // attach the module template markup to the DOM
-        $(anchorSelector).append(baseMarkup);
+    _.templateSettings.variable = 'S';
+    var baseMarkup = $(template);
+    // attach the module template markup to the DOM
+    $(anchorSelector).append(baseMarkup);
 
-        var indexView = function(){
-            var targetLayoutView = new View.IndexLayout();
+    var indexView = function () {
+      var homeLayoutView = new View.HomeLayout();
 
-            var indexView = new View.IndexView();
+      var homeView = new View.helloWorldView();
 
-            var indexContainerRegion = new Backbone.Marionette.Region({
-                el: ".view-index"
-            });
+      homeLayoutView.on('show', function () {
+        homeLayoutView.homeContentRegion.show(homeView);
+      });
 
-            targetLayoutView.on('show',function(layout){
-                indexContainerRegion.show(indexView);
-            });
+      return homeLayoutView;
+    };
 
-            return targetLayoutView;
-        };
-
-        return{
-            IndexView:indexView
-        };
-    }
+    return{
+      IndexView: indexView
+    };
+  }
 );
--- public/modules/home/home.views.js
+++ public/modules/home/home.views.js
@@ -6,23 +6,22 @@
  * Time: 10:58 PM
  *
  */
-define(['sf1','marionette'],
-    function(sf1, Marionette){
+define(['sf1', 'marionette'],
+  function (sf1, Marionette) {
 
-        var indexView = Backbone.Marionette.CompositeView.extend({
-                template:'#IndexTemplate'
-            }
-        );
-        var indexDefaultLayout = Backbone.Marionette.Layout.extend({
-            template:'#IndexLayoutTemplate',
-            regions:{
-                container:'.view-index',
-                recentPostRegion:'[data-region="recentPostRegion"]'
-            }
-        });
-        return {
-            IndexView:indexView,
-            IndexLayout:indexDefaultLayout
-        };
-    }
+    var helloWorldView = Backbone.Marionette.ItemView.extend({
+        template: '#IndexTemplate'
+      }
+    );
+    var homeDefaultLayout = Backbone.Marionette.Layout.extend({
+      template: '#HomeLayoutTemplate',
+      regions: {
+        homeContentRegion: '[data-region="homeContentRegion"]'
+      }
+    });
+    return {
+      helloWorldView: helloWorldView,
+      HomeLayout: homeDefaultLayout
+    };
+  }
 );
